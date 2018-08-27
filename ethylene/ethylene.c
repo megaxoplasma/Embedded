f8bc3a6743baef4af8fb74fc15d0a8932afb5b3a
megaxoplasma <yjz210@nyu.edu>
2018-08-27 16:06:57 -0400

    check

--- ethylene/ethylene.c
+++ ethylene/ethylene.c
@@ -16,40 +16,100 @@
 #include <stdio.h>
 #include <math.h>
 
-
 #define OP_READ 0x03
-
-
-
-
 #define ADEN 0x00000001
-//#define VREFINT_CAL_ADDR    0x1FFFF7BA
 
-uint8_t SPI_read(SPI_HandleTypeDef hspi, uint32_t addr){
+uint8_t SPI_read_drive(SPI_HandleTypeDef* hspi,uint8_t addrL,uint8_t addrR){
+       // printf("Read started \r\n");
+         int dummy = 0x00;
+         uint8_t cmd[] = {addrL,0x03,dummy,addrR};
+      
+       uint8_t data_get[5] = {0};
 
-        
-      return 0;
-     
-
-
+        for(int i = 0; i < sizeof(data_get) ; i++){
+          data_get[i] = 27;
 }
 
+       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET); //make CS 0
+        
+        HAL_SPI_TransmitReceive(hspi, (&cmd), &data_get , sizeof(cmd) , HAL_MAX_DELAY); //transmit opcode to read and read address
+        while (HAL_SPI_GetState(hspi) != HAL_SPI_STATE_READY);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET); //make it 1
+       
+     //  for(int i = 0; i < sizeof(data_get) ; i++){
+      // printf("%d index %d \r\n", data_get[i],i);
+      //}
+      
+      //printf("Read finished, it is currently %d \r\n", data_get[3]);
+      cpu_sw_delay(100);
+
+      return data_get[2];
+  }
+
+
+int SPI_write_drive(SPI_HandleTypeDef* hspi, uint8_t addrL, uint8_t addrR, uint8_t data){
+
+       printf("write started \r\n");
+       uint8_t cmdW1[] = {0x00,0x06,0x00,0x00}; //ENABLE WRITE LATCH
+         
+       uint8_t dummy[10] = {0};
+
+       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET); //make CS 0
+     
+        HAL_SPI_TransmitReceive(hspi, (&cmdW1), &dummy , sizeof(cmdW1) , HAL_MAX_DELAY); //transmit opcode to read and read address
+        while (HAL_SPI_GetState(hspi) != HAL_SPI_STATE_READY);
+      
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET); //make it 1
+        cpu_sw_delay(100);
+
+     
+
+       ///////////////////        WRITE THE DATA
+        uint8_t cmdW2[] = {addrL,0x02, data ,addrR};   
+        
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET); //make CS 0
+        
+        HAL_SPI_TransmitReceive(hspi, (&cmdW2), &dummy , sizeof(cmdW2) , HAL_MAX_DELAY); //transmit opcode to read and read address
+        while (HAL_SPI_GetState(hspi) != HAL_SPI_STATE_READY);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET); //make it 1
+
+        cpu_sw_delay(100);
+
+        ////////////CLOSE WRITE LATCH
+
+        uint8_t cmdW3[] = {0x00,0x04,0x00,0x00}; //ENABLE WRITE LATCH
+
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET); //make CS 0
+        
+        HAL_SPI_TransmitReceive(hspi, (&cmdW3), &dummy , sizeof(cmdW3) , HAL_MAX_DELAY); //transmit opcode to read and read address
+        while (HAL_SPI_GetState(hspi) != HAL_SPI_STATE_READY);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET); //make it 1
+        printf("write finished\r\n");
+        cpu_sw_delay(100);
+    
+        return 0;
+}
+
+void SPI_write(SPI_HandleTypeDef* hspi, uint16_t addr , uint8_t data){
+   uint8_t addrL = (addr & 0x1100) >> 8;
+   uint8_t addrR = (addr & 0x0011);
+   SPI_write_drive(hspi,addrL,addrR,data);
+}
+
+uint8_t SPI_read(SPI_HandleTypeDef* hspi, uint16_t addr){
+   uint8_t addrL = (addr & 0x1100) >> 8;
+   uint8_t addrR = (addr & 0x0011);
+   uint8_t data = SPI_read_drive(hspi,addrL,addrR);
+   return data;
+}
 
 
 
 int main()
 {
 
-  //OPCODE_WREN   = 0b0110,     /* Set Enable Latch */
-  //OPCODE_WRDI   = 0b0100,     /* Reset Write Enable Latch */
-  //OPCODE_RDSR   = 0b0101,     /* Read Status Register */
-  //OPCODE_WRSR   = 0b0001,     /* Write Status Register */
- 
-  //OPCODE_WRITE  = 0b0010,     /* Write Memory */
-  //OPCODE_RDID   = 0b10011111  /* Read Device ID */
-  /*
-    Initialize the PLL, clock tree to all peripherals, flash and Systick 1 ms time reference:
-   */
+
+
   cpu_init();
 
    uart_debug_init(); //initialize PA9 and PA10,           9 is Rx ,10 is Tx
@@ -89,12 +149,12 @@ int main()
   
   printf("YAY\r\n");
 
-//ADC_HandleTypeDef hADC1 = ADC_setup(); //sets up , enables, and starts ADC1 channel 1, which is PA0 
+ADC_HandleTypeDef hADC1 = ADC_setup(); //sets up , enables, and starts ADC1 channel 1, which is PA0 
 
 /////////////////////////////////////////SET UP SPI STUFF///////////////////////////////////////////////////////////////////////
 
 
-	__SPI1_CLK_ENABLE(); //enable SPI1
+	  __SPI1_CLK_ENABLE(); //enable SPI1
     __HAL_RCC_SPI1_CLK_ENABLE();
     SPI_HandleTypeDef hspi;
     hspi.Instance = SPI1;
@@ -143,94 +203,29 @@ while (HAL_SPI_GetState(&hspi) != HAL_SPI_STATE_READY);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 uint32_t value;
-
+uint8_t spi_readd;
 
 /////////////////////////////READ WORKS/////////////////
-        
-        uint8_t cmd[] = {0x00,0x03,0x00,0x00};
-      
-      
-        uint8_t data_get[5] = {0};
+/*
+spi_readd = SPI_read(&hspi,0x0011);
+printf("The result read is %d \r\n",spi_readd);
 
-        for(int i = 0; i < sizeof(data_get) ; i++){
-          data_get[i] = 27;
-}
-
-       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET); //make CS 0
-        
-        HAL_SPI_TransmitReceive(&hspi, (&cmd), &data_get , sizeof(cmd) , HAL_MAX_DELAY); //transmit opcode to read and read address
-        while (HAL_SPI_GetState(&hspi) != HAL_SPI_STATE_READY);
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET); //make it 1
-       
-      for(int i = 0; i < sizeof(data_get) ; i++){
-
-        printf("%d index %d \r\n", data_get[i],i);
-      }
-      printf("Initial read finished, it is currently %d \r\n", data_get[2]);
-      cpu_sw_delay(100);
        ////////////////////////////////////////////WRITE///////////////////
-        printf("TIME TO WRITE \r\n");
-uint8_t cmdW1[] = {0x00,0x06,0x00,0x00}; //ENABLE WRITE LATCH
-         
-      uint8_t dummy[10] = {0};
 
-       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET); //make CS 0
-        
-        HAL_SPI_TransmitReceive(&hspi, (&cmdW1), &dummy , sizeof(cmdW1) , HAL_MAX_DELAY); //transmit opcode to read and read address
-        while (HAL_SPI_GetState(&hspi) != HAL_SPI_STATE_READY);
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET); //make it 1
-         cpu_sw_delay(100);
+SPI_write(&hspi,0x0011,0x1F);
 
-       ///////////////////        WRITE THE DATA
-      uint8_t cmdW2[] = {0x00,0x02,0x25,0x00};   
-        
-       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET); //make CS 0
-        
-        HAL_SPI_TransmitReceive(&hspi, (&cmdW2), &dummy , sizeof(cmdW2) , HAL_MAX_DELAY); //transmit opcode to read and read address
-        while (HAL_SPI_GetState(&hspi) != HAL_SPI_STATE_READY);
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET); //make it 1
+      ///////////////////////read again to confirm
+*/
+//spi_readd = SPI_read(&hspi,0x0011);
 
- cpu_sw_delay(100);
-        ////////////CLOSE WRITE LATCH
-
-        uint8_t cmdW3[] = {0x00,0x04,0x00,0x00}; //ENABLE WRITE LATCH
-
-       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET); //make CS 0
-        
-        HAL_SPI_TransmitReceive(&hspi, (&cmdW3), &dummy , sizeof(cmdW3) , HAL_MAX_DELAY); //transmit opcode to read and read address
-        while (HAL_SPI_GetState(&hspi) != HAL_SPI_STATE_READY);
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET); //make it 1
-        printf("write finished, lets read \r\n");
- cpu_sw_delay(100);
-       
+//printf("The result read is %d \r\n",spi_readd);
 
 
-  ///////////////////////////////////////////////////TEST //////////////////////////////////////////
-
-        uint8_t cmd2[] = {0x00,0x03,0x00,0x00};
-      
-      
-        uint8_t data_get2[5] = {0};
-
-        for(int i = 0; i < sizeof(data_get2) ; i++){
-          data_get2[i] = 27;
-}
-
-       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET); //make CS 0
-      
-        HAL_SPI_TransmitReceive(&hspi, (&cmd2), &data_get2 , sizeof(cmd2) , HAL_MAX_DELAY); //transmit opcode to read and read address
-        while (HAL_SPI_GetState(&hspi) != HAL_SPI_STATE_READY);
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET); //make it 1
-       
-      for(int i = 0; i < sizeof(data_get2) ; i++){
-
-        printf("%d index %d \r\n", data_get2[i],i);
-      }
-
-printf("final read compelte, is it right? IT SHOULD SAYYYYYYYY %d FOR INDEX 2 \r\n", data_get2[2]);
-
-
-
+    uint8_t cmd;
+    uint8_t write_data;
+    uint8_t dataS;
+    uint16_t addr;
+    uint16_t addrS;
 
      
 
@@ -249,35 +244,108 @@ printf("final read compelte, is it right? IT SHOULD SAYYYYYYYY %d FOR INDEX 2 \r
     }
     else
     {
+    
+    printf("Hello, enter 1 to read and 2 to write \r\n");
+    printf("What would you like to do? ");
+     
+    scanf("%d", &cmd);
+    printf("\r\n");
 
-    	
+     switch (cmd)
+     {
+     case 1:
+           printf("What address would you like to read?? \r\n");
+   
+          scanf("%d", &addr);
+          while(addr > 8191){
+                printf("Invalid address,please enter again: \r\n");
+                scanf("%d", &addr);
+                }
+          printf("address is %d \r\n", addr);
+
+         cpu_sw_delay(100U);
+
+         spi_readd = SPI_read(&hspi,addr);
+         printf("The value at address %d is %d \r\n",addr,spi_readd);
+         printf("\r\n");
+          break;
+
+     case 2: 
+          printf("What address would you like to write to?? \r \n");
+          scanf("%d", &addr );
+           cpu_sw_delay(100U);
+           while(addr > 8191){
+                printf("Invalid address,please enter again: \r\n");
+                scanf("%d", &addr);
+                }
+
+          printf("address is %d \r\n", addr);
+          addrS = addr;
+
+          printf("What is the data to write?? \r\n");
+          scanf("%d", &write_data);
+           //printf("addr is currently %d ", addrS);
+            while(write_data > 255){
+                printf("Data value too large,please enter again: \r\n");
+                scanf("%d", &write_data);
+                }
+          cpu_sw_delay(100U);
+          dataS = write_data;
+
+          printf("Data to be written to location %d is %d \r\n", addrS, dataS);
+          cpu_sw_delay(100U);
+          SPI_write(&hspi,addrS,dataS);
+          printf("Check... \r\n");
+          
+          spi_readd = SPI_read(&hspi,addrS);
+          printf("The value at address %d is now %d \r\n",addrS,spi_readd);
+          printf("\r\n");
+          break;
+
+
+
+          cpu_sw_delay(100U);
+         // printf("The value at address %d is %d", addr,spi_data );
+          default:
+          break;
+      }
+    
+
+
+
+
+
+
+
+
+
 
     	//start conversion
- 	 // if( HAL_ADC_PollForConversion(&hADC1,10000)  == HAL_OK )
-//{
-//	   value = HAL_ADC_GetValue(&hADC1);
-//}
+ 	  if( HAL_ADC_PollForConversion(&hADC1,10000)  == HAL_OK )
+{
+	   value = HAL_ADC_GetValue(&hADC1);
+}
 	//float vdd = 3.3 * v_cal / value;
 
 	float voltage;
-	voltage = 1500.0 * (float)value  / 4095.0  ;
+  voltage = 1500.0 * (float)value  / 4095.0  ;
 	//float Vdd = 3300.0 * (ref) / value;
 
 
 
    //  uint32_t temperatureC = (voltage - 0.5) * 100 ;
-      cpu_sw_delay(50U);  // Invoke a simple software busy-wait routine to delay approximately 50 milliseconds
+   //   cpu_sw_delay(50U);  // Invoke a simple software busy-wait routine to delay approximately 50 milliseconds
       //save conversion
 
   
 HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_SET);
    
     
-      cpu_sw_delay(100U);
+     cpu_sw_delay(100U);
       //print conversion
-      //voltage *= 100;
-      //voltage = roundf(voltage);
-      //voltage /= 100;
+      voltage *= 100;
+      voltage = roundf(voltage);
+      voltage /= 100;
  
      //printf("%f millivolts \r\n",voltage);
       ++i; // Increment i for the next test iteration...
